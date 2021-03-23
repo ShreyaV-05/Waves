f6768ed4c508c378e885f7d44d8aa1871a50bcd8
ryanluo <ryanluo42@gmail.com>
2021-03-23 11:24:41 -0700

    app.py

--- .idea/app.py
+++ .idea/app.py
@@ -14,3 +14,5 @@ def home():
 if __name__ == "__main__":
     #runs the application on the repl development server
     app.run(debug=True, port='5000', host='127.0.0.1')
+
+    delete this when u see it
