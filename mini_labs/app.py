9111e2b0d7a41b7d334a9c3046ee3ba3a5a9cc63
andrea abed <andrea.abed@yahoo.com>
2021-03-27 14:09:04 -0700

    add mini lab section

---
+++ mini_labs/app.py
@@ -0,0 +1,10 @@
+from flask import Blueprint
+
+groc_bp = Blueprint('groc', __name__,
+                    template_folder='templates',
+                    static_folder='static', static_url_path='assets')
+
+
+@groc_bp.route('/')
+def index():
+    return ""
