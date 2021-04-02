66ba4c4e6ee5efbdac6a3fdc99435aa69bc50559
yeo88 <70968498+yeo88@users.noreply.github.com>
2021-04-02 13:22:06 -0700

    linked to navbar - not working

--- minilabs_diane/app.py
+++ minilabs_diane/app.py
@@ -2,13 +2,13 @@ from flask import Blueprint, render_template, request
 
 from minilabs_diane.minilab_fib import Cubed
 
-minilabs_diane_bp = Blueprint('minilabs_diane', __name__,
+minilabs_diane_bp = Blueprint('minilabs_diane_bp', __name__,
                                template_folder='templates',
                                static_folder='static', static_url_path='assets')
 
 
-@minilabs_diane_bp.route('/fibonacci', methods=["GET", "POST"])
-def fibonacci():
+@minilabs_diane_bp.route('/fib', methods=["GET", "POST"])
+def cubed():
     if request.form:
-        return render_template("minilabs_diane/fib.html", fibonacci=Cubed(int(request.form.get("series"))))
-    return render_template("minilabs_diane/fib.html", fibonacci=Cubed(2))
+        return render_template("fib.html", fibonacci=Cubed(int(request.form.get("series"))))
+    return render_template("fib.html", fibonacci=Cubed(2))
