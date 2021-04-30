00150c83a73a27e550782593e423ac456146e5b7
ShreyaV-05 <shreya.vasant@yahoo.com>
2021-04-30 13:35:18 -0700

    minilab

--- mini_labs/bubblesort/bubblesort.py
+++ mini_labs/bubblesort/bubblesort.py
@@ -24,14 +24,14 @@ def bubbleSort(sort):
 
 
     # Traverse through all array elements
-    for i in range():
+    for i in range(6):
 
         # Last i elements are already in place
-        for j in range(0, n-i-1):
+        for j in range(0, 6-i-1):
 
             # traverse the array from 0 to n-i-1
             # Swap if the element found is greater
             # than the next element
-            if arr[j] > arr[j+1] :
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+            if sort[j] > sort[j+1] :
+                sort[j], sort[j+1] = sort[j+1], sort[j]
 
