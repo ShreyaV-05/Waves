deb1be368b0aff8d6125ce115244afb78ca46e92
ShreyaV-05 <shreya.vasant@yahoo.com>
2021-04-30 13:59:50 -0700

    minilab

--- bubblesort/bubblesort.py
+++ bubblesort/bubblesort.py
@@ -15,10 +15,18 @@ def find_type():
             string.append(x)
         if type(x) == chr:
             chara.append(x)
-    bubbllestringsort(string)
+    bubblestringsort(string)
     bubblecharasort(chara)
     bubbleintsort(int)
-        """
+    print ("Sorted input is:")
+    for i in range(len(string)):
+        print ("%d" %string[i])
+    for i in range(len(string)):
+        print ("%d" %string[i])
+    for i in range(len(string)):
+        print ("%d" %string[i])
+
+"""
 for y in string: #if string find length of each string and sort depending on length
     z = len(y)
     sort.append(z)
@@ -26,10 +34,11 @@ for u in chara: #if char find ascii value and sort depending on asci value
     v = sort.append(ord(u))
 """
 def bubbleintsort(sort):
+    n=len(sort)
     # Traverse through all array elements
-    for i in range(6):
+    for i in range(n-1):
         # Last i elements are already in place
-        for j in range(0, 6-i-1):
+        for j in range(0, n-i-1):
             # traverse the array from 0 to n-i-1
             # Swap if the element found is greater
             # than the next element
@@ -46,4 +55,13 @@ def bubblestringsort(sort):
             # than the next element
             if sort[len(j)] > sort[len(j+1)] :
                 sort[len(j)], sort[len(j+1)] = sort[len(j+1)], sort[len(j)]
-
+def bubblecharasort(sort):
+    n=len(sort)
+    for i in range(n-1):
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+            # traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if sort[ord(j)] > sort[ord(j+1)] :
+                sort[ord(j)], sort[ord(j+1)] = sort[ord(j+1)], sort[ord(j)]
