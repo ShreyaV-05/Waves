e9719a114fb55daa9ee6a6bfd4975bf8ba0910f3
andrea abed <andrea.abed@yahoo.com>
2021-05-15 05:56:16 -0700

    andrea page

--- movie/app.py
+++ movie/app.py
@@ -1,18 +1,9 @@
 from flask import Blueprint
-from flask import Blueprint, render_template, request
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, request
 
 movie_bp = Blueprint('movie', __name__,
                      template_folder='templates',
                      static_folder='static', static_url_path='assets')
-
-
 @movie_bp.route('/')
-def index():
-    return render_template("base.html")
-
-
-@movie_bp.route('/movies', methods=["GET", "POST"])
-def movies():
-    if request.form:
-        return render_template("movie/moviefront.html")
+def bookstore():
+    return render_template("theatres.html")
